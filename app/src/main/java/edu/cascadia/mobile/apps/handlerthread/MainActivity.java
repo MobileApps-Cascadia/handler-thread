e10b03c5ebfa374601b3ea735d94a451e9c28827
Brian Bansenauer <brianb@cascadia.edu>
2019-11-21 13:21:22 -0800

    initial with TODOs

---
+++ app/src/main/java/edu/cascadia/mobile/apps/handlerthread/MainActivity.java
@@ -0,0 +1,61 @@
+package edu.cascadia.mobile.apps.handlerthread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Worker worker;
+    private TextView textDisplay;
+    private Handler uiHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            //TODO: Use the msg object to set the textDisplay value
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textDisplay = (TextView) findViewById(R.id.textDisplay);
+
+        //Create a new HandlerThread
+        worker = new Worker();
+        worker.execute( new Runnable() {
+            int count=0;
+            @Override
+            public void run() {
+                while(true) { //keep running till the thread stops
+                    try {
+                        Thread.sleep(1000); // sleep for 1 seconds
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Message message = Message.obtain();
+                    //TODO: Update the message.obj with the current count
+
+                    //TODO: Send the message to the uiHandler message queue
+
+                    //Updates the count by 1
+                    count+=1;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy(){
+        worker.quitSafely();
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/edu/cascadia/mobile/apps/handlerthread/Worker.java
@@ -0,0 +1,24 @@
+package edu.cascadia.mobile.apps.handlerthread;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.HandlerThread;
+
+
+public class Worker extends HandlerThread {
+    private static final String THREADNAME = "Worker";
+
+    private Handler handler;
+
+    public Worker(){
+        super(THREADNAME);
+        start(); // Begin the thread's looper
+        handler = new Handler(getLooper()); //assigns a handler Worker's looper
+    }
+
+    public Worker execute(Runnable myTask){
+
+        handler.post(myTask); //Handler places the task onto the Thread's message queue
+        return this;
+    }
+}
